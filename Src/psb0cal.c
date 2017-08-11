d7eaec762f944716abd93f9d80fde06437ee491e
diesisfox <james.zhi.liu@gmail.com>
2017-08-11 00:57:58 -0400

    updated psb cal

--- Src/psb0cal.c
+++ Src/psb0cal.c
@@ -18,7 +18,9 @@ psb0ch5 linear fit: y = 324,696.617028814000x - 106,756.513698630000
 //returns microVolts
 int32_t psb0ch0Map(int32_t raw){
     if(raw&0x800000) raw -= 0x1000000;
-	return (int32_t)roundivide(((int64_t)raw*1000000000 - 15232021526172),32867086);
+	// return (int32_t)roundivide(((int64_t)raw*1000000000 - 15232021526172),32867086);
+	//y = 33,757.579997171900x + 17,055.267227564100
+	return (int32_t)roundivide(((int64_t)raw*1000000000 - 17055267227564),33757579);
 }
 
 //returns microAmps
@@ -31,7 +33,9 @@ int32_t psb0ch1Map(int32_t raw){
 //returns microVolts
 int32_t psb0ch2Map(int32_t raw){
     if(raw&0x800000) raw -= 0x1000000;
-	return (int32_t)roundivide(((int64_t)raw*1000000000 - 11865598039215),32968362);
+	// return (int32_t)roundivide(((int64_t)raw*1000000000 - 11865598039215),32968362);
+	//y = 33,703.698786764700x + 5,835.209252450980
+	return (int32_t)roundivide(((int64_t)raw*1000000000 - 5835209252450),33703698);
 }
 
 //returns microAmps
@@ -44,7 +48,9 @@ int32_t psb0ch3Map(int32_t raw){
 //returns microVolts
 int32_t psb0ch4Map(int32_t raw){
     if(raw&0x800000) raw -= 0x1000000;
-	return (int32_t)roundivide(((int64_t)raw*1000000000 + 11774259803922),33059320);
+	// return (int32_t)roundivide(((int64_t)raw*1000000000 + 11774259803922),33059320);
+	//y = 33,756.891623303200x - 18,138.080411010600
+	return (int32_t)roundivide(((int64_t)raw*1000000000 + 18138080411010),33756892);
 }
 
 //returns microAmps
